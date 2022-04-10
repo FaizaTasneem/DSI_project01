8db738de5149dae9993c5a9d91fc73967c90ebb2
SheikhFahim <sheikh.fahim.zaman@gmail.com>
2022-04-10 16:12:29 +0600

    Added Teacher

--- project1/src/main/java/com/dsi/project1/model/Teacher.java
+++ project1/src/main/java/com/dsi/project1/model/Teacher.java
@@ -1,4 +1,25 @@
 package com.dsi.project1.model;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Setter
+@Getter
+@Entity
 public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int tid;
+    private String name;
+    private String dept;
+    private String gender;
+    private int age;
+
 }
